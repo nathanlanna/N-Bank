9fff0211f346a4d0870c5572b6ea61cf7318073a
nathan costa <nathan.lanna0801@gmail.com>
2023-02-13 20:07:24 -0300

    Começando Novamente

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import "./reset.css";
 
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      saldo: 0,
+      rendimento: 0
+    }
+  }
+  render() {
+    return (
+      <div className='container'>
+        <header className='cabecalho'>
+          <span>CONTA</span>
+          <h1>N-Bank</h1>
+        </header>
+        <main className='dados'>
+            <input className='valor' placeholder='Digite o valor desejado:'></input>
+            <div className='valores'>
+              <div><span>Saldo</span><p>{this.state.saldo}</p></div>
+
+              <div ><span>Rendimento</span><p>{this.state.rendimento}</p></div>
+            </div>
+        </main>
+        <footer className='rodape'>
+          <div className='botoes'>
+            <Button tipo='Sacar'></Button>
+            <Button tipo='Depositar'></Button>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+class Button extends React.Component{
+  constructor(props){
+    super (props)
+  }
+  render(){
+    return <button>{this.props.tipo}</button>
+  }
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout></Layout>
+
     </div>
   );
 }
